254ad4b450a3e493989c038d482128c422b48bb9
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2021-12-19 23:52:31 +0300

    the first lesson for create portfolio

---
+++ src/Main/Main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Main.module.css'
+
+
+function Main() {
+    return <div className={s.mainBlock}>
+        <div className={s.container}>
+<div className={s.text}>
+    <span> HI THERE</span>
+    <h1>I'M A JEFFRY AIRON</h1>
+    <p>A  FRONT-END DEVELOPER</p>
+</div>
+<div className={s.photo}></div>
+        </div>
+</div>
+}
+export default Main;
